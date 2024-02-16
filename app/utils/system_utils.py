e2e211eb8a4370d84e4da5e22ba6f84cd0d46e98
choyakawa <97655818+choyakawa@users.noreply.github.com>
2024-02-17 02:18:29 +0800

    Update system_utils.py

--- app/utils/system_utils.py
+++ app/utils/system_utils.py
@@ -213,7 +213,8 @@ class SystemUtils:
             dest = dest.replace("\\", "/")
             retcode = subprocess.run(['rclone', 'copyto',
                                       src,
-                                      f'NASTOOL:{dest}'],
+                                      f'NASTOOL:{dest}',
+                                      '--drive-upload-cutoff', '1000T'],
                                      startupinfo=SystemUtils.__get_hidden_shell()).returncode
             return retcode, ""
         except Exception as err:
